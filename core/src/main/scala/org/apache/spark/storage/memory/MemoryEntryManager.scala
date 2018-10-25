b9276e34e64e2766aea1164cdc79e4df22dd0ded
cty <123456789>
2018-10-25 15:40:54 +0800

    三大算法接口

--- core/src/main/scala/org/apache/spark/storage/memory/MemoryEntryManager.scala
+++ core/src/main/scala/org/apache/spark/storage/memory/MemoryEntryManager.scala
@@ -17,7 +17,7 @@
 package org.apache.spark.storage.memory
 
 import java.util
-
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 trait MemoryEntryManager[K, V] {
@@ -30,7 +30,7 @@ trait MemoryEntryManager[K, V] {
   def clear()
 
   def containsEntry(key: K): Boolean
-}
+} //接口函数
 
 class FIFOMemoryEntryManager[K, V] extends MemoryEntryManager[K, V] {
   val entries = new util.LinkedHashMap[K, V](32, 0.75f)
@@ -64,7 +64,7 @@ class FIFOMemoryEntryManager[K, V] extends MemoryEntryManager[K, V] {
       entries.containsKey(key)
     }
   }
-}
+}  //FIFO
 
 class LRUMemoryEntryManager[K, V] extends MemoryEntryManager[K, V] {
   def entrySet() : util.Set[util.Map.Entry[K, V]] = {
@@ -102,4 +102,87 @@ class LRUMemoryEntryManager[K, V] extends MemoryEntryManager[K, V] {
       entries.containsKey(key)
     }
   }
+}   //LRU
+
+
+/*
+class MCESCache[K, V] extends MemoryEntryManager[K, V]{
+
+  val byKey = mutable.HashMap[K, MCESItem]()
+  val frequencyHead = FrequencyNode(-1)
+
+  def deleteNode(node: FrequencyNode): Unit = {
+    node.prev.next = node.next
+    node.next.prev = node.prev
+    node.next= null; node.prev = null
+  }
+
+  def access(key: K): Int = {
+    val tmp = this.byKey(key)
+    if (tmp == null) {
+      throw new Exception("No such key")
+    }
+
+    val freq = tmp.parent
+    var nextFreq = freq.next
+
+    if (nextFreq == this.frequencyHead || nextFreq.value != (freq.value + 1)) {
+      nextFreq = MCESCache.getNewNode(freq, nextFreq, freq.value + 1)
+    }
+
+    nextFreq.items += this.byKey(key)
+    tmp.parent = nextFreq
+
+    nextFreq.prev.items -= nextFreq.prev.items.filter(freq => freq.key == key).head
+    if (nextFreq.prev.items.isEmpty){
+      this.deleteNode(freq)
+    }
+
+    tmp.data
+  }
+
+  def insert(key: K, value : Int) = {
+    if (this.byKey.contains(key)){
+      throw new Exception("Key already exists")
+    }
+
+    val freqNode = this.frequencyHead.next
+    if (freqNode.value != 1){
+      this.frequencyHead.next = MCESCache.getNewNode(this.frequencyHead, freqNode)
+      this.byKey(key) = MCESItem(this.frequencyHead.next, value, key)
+      this.frequencyHead.next.items += this.byKey(key)
+    }else{
+      this.byKey(key) = MCESItem(freqNode, value, key)
+      freqNode.items += this.byKey(key)
+    }
+
+  }
+
+  def getMCESItem(): MCESItem = {
+    if (this.byKey.isEmpty) {
+      throw new Exception("The set is empty")
+    }
+    this.byKey(this.frequencyHead.next.items.head.key)
+  }
+}   //MCES双链表
+case class MCESItem(var parent: FrequencyNode, data : Int, key:= K.randomUUID()){ }
+
+case class FrequencyNode(value: Int = 1) {
+  val items: mutable.ArrayBuffer[MCESItem] = mutable.ArrayBuffer[MCESItem]()
+  var prev: FrequencyNode = this
+  var next: FrequencyNode = this
 }
+
+object MCESCache {
+
+  def getNewNode(prev: FrequencyNode, next: FrequencyNode, freqValue: Int = 1): FrequencyNode = {
+    val node = FrequencyNode(freqValue)
+    node.prev = prev
+    node.next = next
+    node.prev.next = node
+    node.next.prev = node
+    node
+  }
+
+}
+*/
